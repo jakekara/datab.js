7bf81b1716bd16acde214beda40bd5d035dc1c5f
Jake Kara <jkara@g.harvard.edu>
2017-06-25 15:39:01 -0400

    Still migrating to Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  // tell rollup our main entry point
+    // tell rollup our main entry point
     entry: 'src/datab.js',
     dest: 'dist/datab.rollup-bundle.js',
-    format: 'iife',
+    // format: 'iife',
+    format: "umd",
     moduleName: 'datab',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    uglify()
-  ]
+    // external:['d3'],
+    // external: id => /d3/.test(id),
+    plugins: [
+	json(),
+	resolve(),
+	babel({
+	    exclude: 'node_modules/**'
+	}),
+	uglify()
+    ]
 }
